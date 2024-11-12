68a2e5aa83edc8751c7897ca71a5dc45a0274e11
AMEGprojects <el.barraem@gmail.com>
2024-11-12 23:02:05 +0200

    star-rating

---
+++ stars/src/App.js
@@ -0,0 +1,18 @@
+
+
+
+import StarRating from './components/Star-Rating';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+<StarRating noOfStars={10}/>
+
+    </div>
+  );
+}
+
+export default App;
